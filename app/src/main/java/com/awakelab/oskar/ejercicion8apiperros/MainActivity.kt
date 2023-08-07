86a0c1fd1b21c54e15545f326c4a2c74f201f1e2
OskarUno <oorellana80@gmail.com>
2023-08-07 19:55:13 -0400

    Initial commit

---
+++ app/src/main/java/com/awakelab/oskar/ejercicion8apiperros/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.awakelab.oskar.ejercicion8apiperros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+[X] Permiso de internet
+[X] Dependencias: Coil, Room, Retrofit, navigation, viewModel
+[] 2 pantallas: Listado y detalle
+[] ViewBinding
+[] navigation
+[] Listado: RecyclerView + ViewHolder + Adapter
+[] Detalle: RecyclerView + ViewHolder + Adapter
+[] Repositorio
+[] Remoto:
+    [] Dataclass
+    [] API (interfaz)
+    [] Retrofit
+[] Local:
+    [] Database
+    [] Entity
+    [] DAO
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
